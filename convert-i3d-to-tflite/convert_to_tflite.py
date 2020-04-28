07298e505e053dc88ec9e7de3f56643460d7c8c8
kimia khabiri <kimia.khabiri.92@gmail.com>
2020-04-28 20:02:17 +0430

    Convert saved_model of i3d-kinetics to tflite

---
+++ convert-i3d-to-tflite/convert_to_tflite.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+
+converter = tf.lite.TFLiteConverter.from_saved_model('Inflated 3D', tags=[[], ['train']])
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS,
+                                       tf.lite.OpsSet.SELECT_TF_OPS]
+tflite_model = converter.convert()
+open("converted_model.tflite", "wb").write(tflite_model)
