1db092e5ce7452e20e3d82ca9b5ab58ce3c083d0
kimia khabiri <kimia.khabiri.92@gmail.com>
2020-07-31 20:19:09 +0430

    Change .tflite file

--- convert-i3d-to-tflite/convert_to_tflite.py
+++ convert-i3d-to-tflite/convert_to_tflite.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 converter = tf.lite.TFLiteConverter.from_saved_model('Inflated 3D', tags=[[], ['train']])
-converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS,
-                                       tf.lite.OpsSet.SELECT_TF_OPS]
+converter.target_spec.supported_ops = [tf.lite.OpsSet.SELECT_TF_OPS]
+converter.optimizations = [tf.lite.Optimize.DEFAULT]                                  
 tflite_model = converter.convert()
 open("converted_model2.tflite", "wb").write(tflite_model)
